c3369c3dabd60ea41d86f47d8d89ceda7a4a0780
matheus <matheusml90@gmail.com>
2012-08-05 21:56:56 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InvestBrasil::Application.config.secret_token = 'a8c70358efee42cddbf763cac7c72ca5cd83f7417cdc43fc7790bc61c9e82d6dc5815587be7dfc06089017110faa1a88cd4b96ea75bb88904b05e093157f8ee0'
