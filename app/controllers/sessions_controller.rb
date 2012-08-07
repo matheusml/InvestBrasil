725f1b2aa419c02e2f64c774401abbb7e19a4ab8
matheusml <matheusml90@gmail.com>
2012-08-07 14:07:51 -0300

    Removes the facebook hash #_=_ callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,8 @@
 class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
-    puts "--- #{user.inspect}"
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to start_path
   end
 
   def destroy
