0821644e0e68818531509c13eb7ae45330945eae
matheusml <matheusml90@gmail.com>
2012-08-07 14:46:59 -0300

    Adds companies controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ InvestBrasil::Application.routes.draw do
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resources :companies
 end
