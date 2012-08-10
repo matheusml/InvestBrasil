0cca799b8062aa6e145a870abbf13833bbe2d398
matheusml <matheusml90@gmail.com>
2012-08-10 14:03:51 -0300

    Adds Comments model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120807004404) do
+ActiveRecord::Schema.define(:version => 20120810165027) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "content"
+    t.integer  "company_users_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "companies", :force => true do |t|
     t.string   "name"
