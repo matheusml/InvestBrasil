b5d080576a5a6e40c905ec8523b50e8e2c062c62
matheusml <matheusml90@gmail.com>
2012-08-10 13:45:52 -0300

    Adds CRUD to companies

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,4 +6,39 @@ class CompaniesController < ApplicationController
 	def show
 		@company = Company.find params[:id]
 	end
+
+	def new
+		@company = Company.new
+	end
+
+	def create
+		@company = Company.new params[:company]
+		
+		if @company.save
+			redirect_to companies_path, :notice => 'Empresa criada com sucesso'			
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@company = Company.find params[:id]
+	end
+
+	def update
+		@company = Company.find params[:id]
+		
+		if @company.update_attributes params[:company]
+			redirect_to companies_path, :notice => 'Empresa atualizada com sucesso'			
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		company = Company.find params[:id]
+		company.destroy
+
+		redirect_to companies_path, :notice => 'Empresa deletada com sucesso'
+	end
 end
