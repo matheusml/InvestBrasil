a18f028f881e45662de801f053b627cdc4479a92
matheusml <matheusml90@gmail.com>
2012-08-18 13:39:48 -0300

    Makes users to automatically login when signed up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,10 +10,9 @@ class SessionsController < ApplicationController
 
   def create
   	user = User.authenticate(params[:email], params[:password])
-    
 		if user
 			session[:user_id] = user.id
-			redirect_to root_url, :notice => 'Logged in'
+			redirect_to root_url, :notice => 'Logado com sucesso'
 		else
 			flash.now.alert = "Invalid email or password"
 			render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,8 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new params[:user]
 		if @user.save
-			redirect_to root_url, :notice => 'Signed Up!'
+			session[:user_id] = @user.id
+			redirect_to root_url
 		else
 			render 'new'
 		end
