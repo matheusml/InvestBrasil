461662dfb2f4e7dbf08137da98bf350937e0506b
matheusml <matheusml90@gmail.com>
2012-08-18 13:25:19 -0300

    Adds admin field to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120818015453) do
+ActiveRecord::Schema.define(:version => 20120818162320) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
