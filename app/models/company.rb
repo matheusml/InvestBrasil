a48c3044eb128e4c54a3cb387dd3caab57265e53
matheusml <matheusml90@gmail.com>
2012-08-25 19:13:38 -0300

    Adds image to company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,5 @@
 class Company < ActiveRecord::Base
+	attr_accessible :avatar
+  has_attached_file :avatar, :styles => { :medium => "150x150>", :thumb => "32x32>" }
 	has_many :company_users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120825165923) do
+ActiveRecord::Schema.define(:version => 20120825220521) do
 
   create_table "comments", :force => true do |t|
     t.text     "content"
@@ -25,6 +25,10 @@ ActiveRecord::Schema.define(:version => 20120825165923) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "company_users", :force => true do |t|
