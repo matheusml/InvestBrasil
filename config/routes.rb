7676680dbeaa18680561ac7ea3afdfc2ffd36184
matheusml <matheusml90@gmail.com>
2012-09-07 16:43:58 -0300

    Adds notification routes

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ InvestBrasil::Application.routes.draw do
   
   resources :users
   resources :sessions
+  resources :notifications
 
   match "/companies_ajax" => "companies#companies_ajax"
   match "/redirect_to_correct_company" => "companies#redirect_to_correct_company",
