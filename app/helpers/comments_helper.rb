3545f3427a3eb6c947f2476680e273a7d51aee39
matheusml <matheusml90@gmail.com>
2012-09-23 11:59:47 -0300

    Disable comment if user is not logged in

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,27 @@
+#encoding: utf-8
+
 module CommentsHelper
+	def placeholder_condition user
+		if user
+			'Entre com seu comentário'
+		else
+			'Cadastre-se para poder comentar'
+		end
+	end
+
+	def disabled? user
+		if user
+			''
+		else
+			'disabled'
+		end
+	end
+
+	def disabled_if_no_user user
+		if user
+			false
+		else
+			true
+		end
+	end
 end
